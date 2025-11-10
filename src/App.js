bbf217f02bac6a61cb670ca31683fa24f86da92d
Sanjitha Dilshan <sanjudilshan7233@gmail.com>
2025-11-10 13:51:07 +0530

    Handler Functions

--- src/App.js
+++ src/App.js
@@ -3,28 +3,36 @@ import StudentCard from "./components/StudentCard"; // make sure path is correct
 import "./App.css";
 
 function App() {
+  // const input = document.getElementById("normal-text-2");
+  // input.addEventListener("input", function () {
+  //   console.log(input.value);
+  // });
+
   const handleChange = (event) => {
     console.log(event.target.value); // fixed typo
   };
 
-  const students = [
-    { name: "Amal", age: 20, homeTown: "Gampaha" },
-    { name: "Kamal", age: 25, homeTown: "Colombo" },
-    { name: "Nimal", age: 18, homeTown: "Negombo" },
-  ];
+  // const students = [
+  //   { name: "Amal", age: 20, homeTown: "Gampaha" },
+  //   { name: "Kamal", age: 25, homeTown: "Colombo" },
+  //   { name: "Nimal", age: 18, homeTown: "Negombo" },
+  // ];
 
   return (
     <div>
       <h1>My React App</h1>
       <input type="text" id="normal-text-2" onChange={handleChange} />
-      {students.map((student, index) => (
+      {/* {students.map((student, index) => (
         <StudentCard
           key={index}
           name={student.name}
           age={student.age}
           homeTown={student.homeTown}
         />
-      ))}
+      ))} */}
+      <StudentCard name="Amal" age={20} homeTown="Gampaha" />
+      <StudentCard name="Kamal" age={25} homeTown="Colombo" />
+      <StudentCard name="Nimal" age={18} homeTown="Negombo" />
     </div>
   );
 }
