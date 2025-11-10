17a0653d9a17663f257c6236d5a8fcb32ed1dfd0
Sanjitha Dilshan <sanjudilshan7233@gmail.com>
2025-11-10 10:24:17 +0530

    Props & Prop Types

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,11 +1,20 @@
-const Student = () => {
+import React from "react";
+import PropTypes from "prop-types";
+
+const Student = ({ name, age, homeTown }) => {
   return (
     <div>
       <h3>{name}</h3>
-      <h3>20</h3>
-      <h3>Gampaha</h3>
+      <h3>{age}</h3>
+      <h3>{homeTown}</h3>
     </div>
   );
+};
+
+Student.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+  homeTown: PropTypes.string,
 };
 
 export default Student;
--- src/components/StudentCard.jsx
+++ src/components/StudentCard.jsx
@@ -1,19 +1,27 @@
+import React from "react";
 import Student from "../components/Student";
+import PropTypes from "prop-types";
 
 const StudentCard = ({ name, age, homeTown }) => {
   return (
-    <div>
+    <div className="st-card">
       <img
         src="https://png.pngtree.com/png-vector/20220610/ourmid/pngtree-kid-student-back-to-school-in-uniform-wear-backpack-png-image_4938811.png"
-        alt="Student Image"
+        alt={"Student Image_${name}"}
         className="student-image"
       />
-      <h3>{name}</h3>
+      {/* <h3>{name}</h3>
       <h3>{age}</h3>
-      <h3>{homeTown}</h3>
-      <Student />
+      <h3>{homeTown}</h3> */}
+      <Student name={name} age={age} homeTown={homeTown} />
     </div>
   );
+};
+
+StudentCard.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+  homeTown: PropTypes.string,
 };
 
 export default StudentCard;
