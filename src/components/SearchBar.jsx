c12228ef9421c4cebe9443b12628485b6a162b62
Sanjitha Dilshan <sanjudilshan7233@gmail.com>
2025-12-03 23:02:28 +0530

    SearchBar component convert functional to class component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 
-const SearchBar = ({ onSearch, searchText }) => {
-  const searchHandle = (event) => {
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  searchHandle = (event) => {
     // console.log(event.target.value);
-    onSearch(event);
+    this.props.onSearch(event);
   };
-  return (
-    <div className="form-container">
-      <form className="form-wrapper">
-        <input
-          type="text"
-          id="search"
-          value={searchText}
-          placeholder="Search for..."
-          onChange={searchHandle}
-        />
-      </form>
-    </div>
-  );
-};
+
+  render() {
+    const { searchText } = this.props;
+    return (
+      <div className="form-container">
+        <form className="form-wrapper">
+          <input
+            type="text"
+            id="search"
+            value={searchText}
+            placeholder="Search for..."
+            onChange={this.searchHandle}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+// const SearchBar = ({ onSearch, searchText }) => {
+
+//   return (
+
+//   );
+// };
 
 export default SearchBar;
