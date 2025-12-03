bd0cb22aa15581e7ec8815d12bb6ea7bc2916fcd
Sanjitha Dilshan <sanjudilshan7233@gmail.com>
2025-12-03 21:23:02 +0530

    App.js functional component convert to class component

--- src/App.js
+++ src/App.js
@@ -1,48 +1,63 @@
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
 import StudentCard from "./components/StudentCard"; // make sure path is correct
 import SearchBar from "./components/SearchBar";
 import "./App.css";
 
-function App() {
-  const [text, setText] = useState("");
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+    };
+  }
 
-  const studentList = [
-    { name: "Amal", age: 20, town: "Gampaha" },
-    { name: "Kamal", age: 25, town: "Colombo" },
-    { name: "Nimal", age: 18, town: "Negombo" },
-  ];
-
-  const searchedStudent = studentList.filter((student) =>
-    student.name.toLowerCase().includes(text.toLowerCase())
-  );
-
-  const searchChange = (event) => {
+  searchChange = (event) => {
     console.log(event.target.value);
-    setText(event.target.value);
+    this.setState({ text: event.target.value });
   };
 
-  return (
-    <div>
-      <h1>My React App</h1>
-      <SearchBar onSearch={searchChange} searchText={text} />
+  render() {
+    const studentList = [
+      { name: "Amal", age: 20, town: "Gampaha" },
+      { name: "Kamal", age: 25, town: "Colombo" },
+      { name: "Nimal", age: 18, town: "Negombo" },
+    ];
 
+    const searchedStudent = studentList.filter((student) =>
+      student.name.toLowerCase().includes(this.state.text.toLowerCase())
+    );
+
+    return (
       <div>
-        <p id="sample text">{text}</p>
-        <p id="sample text-2"></p>
-      </div>
+        <h1>My React App</h1>
+        <SearchBar onSearch={this.searchChange} searchText={this.state.text} />
 
-      {searchedStudent.map((student, index) => {
-        return (
-          <StudentCard
-            key={index}
-            name={student.name}
-            age={student.age}
-            homeTown={student.town}
-          />
-        );
-      })}
-    </div>
-  );
+        <div>
+          <p id="sample text">{this.state.text}</p>
+          <p id="sample text-2"></p>
+        </div>
+
+        {searchedStudent.map((student, index) => {
+          return (
+            <StudentCard
+              key={index}
+              name={student.name}
+              age={student.age}
+              homeTown={student.town}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   const [text, setText] = useState("");
+
+//   return (
+
+//   );
+// }
 
 export default App;
